90c357a776195e7eb3aabbebdae924f7bdf33a9c
mduyen2505 <156625698+mduyen2505@users.noreply.github.com>
2024-11-25 20:24:58 +0700

    Cập nhật giao diện chính

--- main_interface.py
+++ main_interface.py
@@ -20,6 +20,12 @@ root.geometry("500x500")
 # Tạo frame chính
 main_frame = tk.Frame(root, bg='gray')
 main_frame.place(relx=0.5, rely=0.5, anchor='center')
+group_label = tk.Label(main_frame, 
+                       text="HỆ THỐNG NHẬN DẠNG KHUÔN MẶT \nNHÓM 6 - Xử lý ảnh \nGiảng viên: Hồ Văn Quí", 
+                       bg='gray', fg='black', 
+                       font=('Arial', 16, ), 
+                       relief=tk.RAISED, bd=5, padx=20, pady=20)
+group_label.pack(pady=25)
 
 # Tạo và định vị các nút
 btn_dataset = tk.Button(main_frame, text="HÌNH ẢNH", command=run_dataset_with_name, 
